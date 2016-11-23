31424b029219e39d6839064a6b11832e25a82dab
myh <myh@myhNb>
2016-11-23 20:18:15 +0800

    Add code to share screenshot. (support W10M)

--- Taq/MainPage.xaml.cs
+++ Taq/MainPage.xaml.cs
@@ -4,6 +4,11 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 
 using Taq.Views;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -19,7 +24,35 @@ namespace Taq
         public MainPage()
         {
             this.InitializeComponent();
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.GetForCurrentView().DataRequested += MainPage_DataRequested;
             frame.Navigate(typeof(Home));
+        }
+
+        async void MainPage_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            DataRequest request = args.Request;
+            args.Request.Data.SetText("TAQ");
+            args.Request.Data.Properties.Title = Windows.ApplicationModel.Package.Current.DisplayName;
+            RenderTargetBitmap bitmap = new RenderTargetBitmap();
+
+            DataRequestDeferral deferral = request.GetDeferral();
+            await bitmap.RenderAsync(mainPage);
+            IBuffer pixelBuffer = await bitmap.GetPixelsAsync();
+            byte[] pixels = WindowsRuntimeBufferExtensions.ToArray(pixelBuffer, 0, (int)pixelBuffer.Length);
+
+            // 2. Write the pixels to a InMemoryRandomAccessStream
+            var stream = new InMemoryRandomAccessStream();
+            var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, stream);
+
+            encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Straight, (uint)bitmap.PixelWidth, (uint)bitmap.PixelHeight, 96, 96,
+                pixels);
+
+            await encoder.FlushAsync();
+            stream.Seek(0);
+
+            // 3. Share it
+            args.Request.Data.SetBitmap(RandomAccessStreamReference.CreateFromStream(stream));
+            deferral.Complete();
         }
 
         private void HamburgerButton_Click(Object sender, RoutedEventArgs e)
