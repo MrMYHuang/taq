108e3355a94088fc47cb77d110f4e244edb826b4
myh <myh@live.com>
2016-12-08 23:02:42 +0800

    Fix a text layout bug of live tile image if text scaling of OS is used.

--- TaqBackTask/TaqBackTask.cs
+++ TaqBackTask/TaqBackTask.cs
@@ -4,19 +4,7 @@ using Windows.ApplicationModel.Background;
 using Windows.Storage;
 using System.IO;
 using TaqShared;
-using Windows.UI.Core;
-using Windows.ApplicationModel.Core;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.Storage.Streams;
-using Windows.UI.Xaml;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Graphics.Imaging;
-using Windows.Graphics.Display;
-using System.Threading.Tasks;
-using Windows.UI.Xaml.Media;
-using Windows.UI;
-using System.Collections.Generic;
-using Windows.UI.Xaml.Controls;
 
 namespace TaqBackTask
 {
