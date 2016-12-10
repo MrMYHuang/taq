191c03a2546280fb51a49cf587e84a54268e9415
myh <myh@live.com>
2016-12-10 16:12:12 +0800

    Add a missing file.

---
+++ TaqBackTask/BackTaskUpdateTiles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Taq;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace TaqBackTask
+{
+    public sealed class BackTaskUpdateTiles : XamlRenderingBackgroundTask
+    {
+        private static TaqModel m = new TaqModel();
+
+        protected async override void OnRun(IBackgroundTaskInstance taskInstance)
+        {
+            // We assume that this method has a high probability of a successfull run
+            // after a failed run with exceptions. It means the success rate of a run is almost independent of the previous runs. So, we just catch exceptions and do nothing, so that this baskgroundtask won't crash and exit.
+            try
+            {
+                // Get a deferral, to prevent the task from closing prematurely
+                // while asynchronous code is still running.
+                BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+                await m.loadAqXml();
+                m.convertXDoc2Dict();
+                await m.loadCurrSite();
+
+                // Update the live tile with the feed items.
+                await m.updateLiveTile();
+
+                // Inform the system that the task is finished.
+                deferral.Complete();
+            }
+            catch (Exception ex)
+            {
+                // Do nothing.
+            }
+        }
+
+        volatile bool _cancelRequested = false;
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            //
+            // Indicate that the background task is canceled.
+            //
+            _cancelRequested = true;
+        }
+    }
+}
