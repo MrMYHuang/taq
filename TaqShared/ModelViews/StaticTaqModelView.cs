928204c5ad00cb61f9c1e1e7b2d0d7035be8cfd2
myh <myh@myhNb>
2016-12-14 08:27:17 +0800

    Add a missing file.

---
+++ TaqShared/ModelViews/StaticTaqModelView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace TaqShared.ModelViews
+{
+    public class StaticTaqModelView
+    {
+        public async static Task<StorageFile> saveUi2Png(string fileName, UIElement ui)
+        {
+            RenderTargetBitmap bitmap = new RenderTargetBitmap();
+            await bitmap.RenderAsync(ui);
+            IBuffer pixelBuffer = await bitmap.GetPixelsAsync();
+            byte[] pixels = WindowsRuntimeBufferExtensions.ToArray(pixelBuffer, 0, (int)pixelBuffer.Length);
+
+            var saveFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            // Encode the image to the selected file on disk 
+            using (var fileStream = await saveFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream);
+                encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Straight, (uint)bitmap.PixelWidth, (uint)bitmap.PixelHeight, DisplayInformation.GetForCurrentView().LogicalDpi, DisplayInformation.GetForCurrentView().LogicalDpi,
+                pixels);
+                await encoder.FlushAsync();
+            }
+            return saveFile;
+        }
+    }
+}
