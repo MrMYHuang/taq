68cf2867d65fe444ab31072a57c2cfe3a9b9ddc2
myh <myh@myhNb>
2016-12-16 21:48:55 +0800

    Switch colors by UserMapIcon enable status.

--- Taq/Views/UserMapIcon.xaml.cs
+++ Taq/Views/UserMapIcon.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -9,6 +12,18 @@ namespace Taq.Views
         public UserMapIcon()
         {
             this.InitializeComponent();
+        }
+
+        private void UserControl_IsEnabledChanged(object sender, Windows.UI.Xaml.DependencyPropertyChangedEventArgs e)
+        {
+            if (this.IsEnabled)
+            {
+                centerCircle.Fill = Application.Current.Resources["SystemControlHighlightAccentBrush"] as SolidColorBrush;
+            }
+            else
+            {
+                centerCircle.Fill = new SolidColorBrush(Colors.Gray);
+            }
         }
     }
 }
