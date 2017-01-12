733e48beabf9e9b03532ee20df17fec1195fcb8a
myh <myh@live.com>
2017-01-13 07:52:02 +0800

    * Update status messages.

--- Taq/Views/AqSiteMap.xaml.cs
+++ Taq/Views/AqSiteMap.xaml.cs
@@ -84,11 +84,11 @@ namespace Taq.Views
                 var pos = await app.vm.m.geoLoc.GetGeopositionAsync();
                 var p = pos.Coordinate.Point;
                 await posUmi(p);
-                mainPage.statusTextBlock.Text = "定位完成。";
+                mainPage.statusTextBlock.Text = "定位完成。上次資料更新日期：" + app.vm.m.getLastUpdateTime();
             }
             catch (Exception ex)
             {
-                mainPage.statusTextBlock.Text = "定位失敗！";
+                mainPage.statusTextBlock.Text = "定位失敗！上次資料更新日期：" + app.vm.m.getLastUpdateTime();
             }
             return 0;
         }
