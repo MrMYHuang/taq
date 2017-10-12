a94180128a6d96a78a616b411876ffc8405985ca
myh <myh@live.com>
2017-10-12 17:59:50 +0800

    * Add Params.cs.

---
+++ Taq.Shared/Models/Params.cs
@@ -0,0 +1,26 @@
+﻿﻿using System.Text.RegularExpressions;
+using Windows.Security.Authentication.Web;
+namespace Taq.Shared.Models
+{
+    public static class Params
+    {
+        // The AQ data filename.
+        public const string aqDbFile = "taqi.json";
+        // TAQ server URI.
+#if DEBUG
+        public static string uriHost = "http://localhost:1337/";
+#else
+        public static string uriHost = "https://YourTaqServerIp/";
+#endif
+        // Table name of AQ histories.
+        public static string aqHistTabName = "epatw";
+        // Download file name of AQ history.
+        public static string aqHistFile = "AqHist.json";
+        public static string hockeyAppId = "YourHockeyAppId";
+        public static string bingMapToken = "YourBingMapToken";
+		// facebook app ID.
+        public static string auth0Domain = "YourAuth0Domain";
+        public static string auth0ClientId = "YourAuth0ClientId";
+
+    }
+}
